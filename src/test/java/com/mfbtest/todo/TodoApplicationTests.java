381b38d54153e33b7ce846fece4c5907c6555b00
Mark Jaeger <mark.jaeger525@gmail.com>
2026-05-02 14:36:12 +0200

    Generated spring project

---
+++ src/test/java/com/mfbtest/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfbtest.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
